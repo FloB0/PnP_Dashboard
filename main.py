863efed0432dfdf1c9203fe7250285d0cb162e4a
Florian Wasmeier <wasmeierflo@gmail.com>
2023-08-10 20:37:11 +0200

    implemented unique names, dropdown and the form adaption

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ from util import *
 
 
 def app():
+    st.set_page_config(
+        page_title="DarkDystopia",
+        page_icon="🧊",
+        layout="centered",
+        initial_sidebar_state="expanded"
+    )
     st.title("Character Dashboard")
 
     # Log in page
     if 'logged_in' not in st.session_state:
         st.session_state.logged_in = False
 
-    if 'button_clicked' not in st.session_state:
-        st.session_state.button_clicked = False
-
     if not st.session_state.logged_in:
         # Log in page
-        st.session_state.key = st.text_input("Enter your key")
+        names_from_primary_info = get_values_alchemy('primary_info', 'name')
+        st.session_state.key = st.selectbox("Select your character", names_from_primary_info)
 
-        if st.button('Log in') or st.session_state.button_clicked:
+        if st.button('Log in'):
             # Try to get a character with the entered key as the id
-            character = get_character_by_id_alchemy(st.session_state.key)
+            character = get_character_by_name_alchemy(st.session_state.key)
 
             # If a character was found, display their information
             if character is not None:
@@ -43,72 +47,10 @@ def app():
             ss.show_form = not ss.show_form
 
         if ss.show_form:
-            with st.form(key='character_form'):
-                # fetch dropdown data
-                races = get_values_alchemy('race', 'name')
-                #classes = get_values_alchemy('class')
-
-                st.write('Character Details')
-                name = st.text_input('Name')
-                race = st.selectbox('Rasse', races)
-                #class_ = st.selectbox('Klasse', classes)
-
-                c1, c2, c3 = st.columns(3)
-                with c1:
-                    st.write('Pysis')
-                    kk = st.number_input('Körperkraft', value=0, step=1)
-                    a = st.number_input('Ausdauer', value=0, step=1)
-                    p = st.number_input('Präzision', value=0, step=1)
-                    pb = st.number_input('Physische Belastbarkeit', value=0, step=1)
-                    v = st.number_input('Verhindern', value=0, step=1)
-                with c2:
-                    st.write('Psyche')
-                    intel = st.number_input('Intelligenz', value=0, step=1)
-                    wk = st.number_input('Willenskraft', value=0, step=1)
-                    wa = st.number_input('Wahrnehmung', value=0, step=1)
-                    mb = st.number_input('Mentale Belastbarkeit', value=0, step=1)
-                    ins = st.number_input('Inspiration', value=0, step=1)
-                with c3:
-                    st.write('Talente')
-                    ini = st.number_input('Initiative', value=0, step=1)
-                    tv = st.number_input('Technisches Verständnis', value=0, step=1)
-                    g = st.number_input('Glück', value=0, step=1)
-                    wi = st.number_input('Wissen', value=0, step=1)
-                    c = st.number_input('Charisma', value=0, step=1)
-
-                # Create the submit button
-                submitted = st.form_submit_button("Submit")
-
-                # If the submit button is clicked, insert the new character into the SQLite database
-                if submitted:
-                    if name == '':
-                        st.warning('Please enter a name before submitting.')
-                    else:
-                        character = {
-                            'name': name,
-                            'race': race,
-                            #'class': class_,
-                            'kk': kk,
-                            'a': a,
-                            'p': p,
-                            'pb': pb,
-                            'v': v,
-                            'intel': intel,
-                            'wk': wk,
-                            'wa': wa,
-                            'mb': mb,
-                            'ins': ins,
-                            'ini': ini,
-                            'tv': tv,
-                            'g': g,
-                            'wi': wi,
-                            'c': c
-                        }
-                        insert_character_alchemy(character)
-                        st.success('Character created successfully!')
+            ss.show_form = show_character_submit_form()
     else:
         # Display character information
-        character = get_character_by_id_alchemy(st.session_state.key)
+        character = get_character_by_name_alchemy(st.session_state.key)
         st.write('Character Details')
         st.text(f"Name: {character['name']}", help= f"This value is calculated by KK({character['kk']}) + INT({character['intel']})")
         st.text(f"Rasse: {character['race']}")
--- util.py
+++ util.py
@@ -248,7 +248,7 @@ def insert_character_alchemy(character):
         connection.commit()
 
 
-def  get_character_by_id(in_id):
+def get_character_by_id(in_id):
     # Connect to the MySQL database
     conn = init_connection()
     c = conn.cursor()
@@ -330,6 +330,26 @@ def get_character_by_id_alchemy(in_id):
         # If no record was found, return None
     return None
 
+def get_character_by_name_alchemy(in_name):
+    engine = init_connection_alchemy()
+    metadata = MetaData()
+
+    # Reflect the table
+    table = Table('primary_info', metadata, autoload_with=engine)
+
+    # Construct the SELECT statement
+    stmt = select(table).where(table.c.name == in_name)
+
+    # Execute the statement and fetch the result
+    with engine.connect() as connection:
+        result = connection.execute(stmt).fetchone()
+
+    # If a record was found, return it as a dictionary
+    if result is not None:
+        return {result._fields[i]: result[i] for i in range(len(result._fields))}
+
+        # If no record was found, return None
+    return None
 
 def update_secondary(id):
     character = get_character_by_id(id)
@@ -377,6 +397,7 @@ def update_secondary(id):
     }
     update_character('secondary_info', id, updated_values)
 
+
 def update_secondary_alchemy(id):
     character = get_character_by_id_alchemy(id)
     nahkampf = character['kk'] * 2 + character['p'] + character['nahkampf']
@@ -423,4 +444,72 @@ def update_secondary_alchemy(id):
     }
     update_character_alchemy('secondary_info', id, updated_values)
 
+
 # def update_primary(id):
+def show_character_submit_form():
+    with st.form(key='character_form'):
+        # fetch dropdown data
+        races = get_values_alchemy('race', 'name')
+        # classes = get_values_alchemy('class')
+
+        st.write('Character Details')
+        name = st.text_input('Name')
+        race = st.selectbox('Rasse', races)
+        # class_ = st.selectbox('Klasse', classes)
+
+        c1, c2, c3 = st.columns(3)
+        with c1:
+            st.write('Pysis')
+            kk = st.number_input('Körperkraft', value=0, step=1)
+            a = st.number_input('Ausdauer', value=0, step=1)
+            p = st.number_input('Präzision', value=0, step=1)
+            pb = st.number_input('Physische Belastbarkeit', value=0, step=1)
+            v = st.number_input('Verhindern', value=0, step=1)
+        with c2:
+            st.write('Psyche')
+            intel = st.number_input('Intelligenz', value=0, step=1)
+            wk = st.number_input('Willenskraft', value=0, step=1)
+            wa = st.number_input('Wahrnehmung', value=0, step=1)
+            mb = st.number_input('Mentale Belastbarkeit', value=0, step=1)
+            ins = st.number_input('Inspiration', value=0, step=1)
+        with c3:
+            st.write('Talente')
+            ini = st.number_input('Initiative', value=0, step=1)
+            tv = st.number_input('Technisches Verständnis', value=0, step=1)
+            g = st.number_input('Glück', value=0, step=1)
+            wi = st.number_input('Wissen', value=0, step=1)
+            c = st.number_input('Charisma', value=0, step=1)
+
+        # Create the submit button
+        submitted = st.form_submit_button("Submit")
+
+        # If the submit button is clicked, insert the new character into the SQLite database
+        if submitted:
+            if name == '':
+                st.warning('Please enter a name before submitting.')
+            elif name in get_values_alchemy('primary_info', 'name'):
+                st.warning('Name already taken. Please try something else')
+            else:
+                character = {
+                    'name': name,
+                    'race': race,
+                    # 'class': class_,
+                    'kk': kk,
+                    'a': a,
+                    'p': p,
+                    'pb': pb,
+                    'v': v,
+                    'intel': intel,
+                    'wk': wk,
+                    'wa': wa,
+                    'mb': mb,
+                    'ins': ins,
+                    'ini': ini,
+                    'tv': tv,
+                    'g': g,
+                    'wi': wi,
+                    'c': c
+                }
+                insert_character_alchemy(character)
+                st.success('Character created successfully!')
+                return False
