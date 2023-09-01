e6211f8c0b8a067c129e45424bdc2e9c64b731cd
Florian Wasmeier <wasmeierflo@gmail.com>
2023-09-01 23:53:00 +0200

    Edited pagination

--- pages/4_⚙️_Manage_Data.py
+++ pages/4_⚙️_Manage_Data.py
@@ -2,14 +2,14 @@ from pages import (
     insert_character, insert_class, insert_item, insert_race, delete_character, delete_item, delete_race, \
     delete_class, empty_page, insert_stat, delete_stat, update_stat, edit_item, insert_trait, update_trait, edit_trait,
     delete_trait,
-)
+    )
 import streamlit as st
 
 st.set_page_config(
     page_title="DarkDystopia",
     page_icon="⚙️",
     layout="wide",
-)
+    )
 
 page_names_to_funcs = {
     "-": empty_page,
@@ -17,19 +17,19 @@ page_names_to_funcs = {
     "Insert Class": insert_class,
     "Insert Item": insert_item,
     "Insert Race": insert_race,
+    "Insert Trait": insert_trait,
     "Create Stat": insert_stat,
     "Update Stat": update_stat,
+    "Update Trait": update_trait,
+    "Edit Item": edit_item,
+    "Edit Trait": edit_trait,
     "Delete Character": delete_character,
     "Delete Item": delete_item,
     "Delete Race": delete_race,
     "Delete Class": delete_class,
     "Delete Stat": delete_stat,
-    "Edit Item": edit_item,
-    "Insert Trait": insert_trait,
-    "Update Trait": update_trait,
-    "Edit Trait": edit_trait,
     "Delete Trait": delete_trait
-}
+    }
 
 page_name = st.selectbox("Chose what you want to do:", page_names_to_funcs.keys())
 page_names_to_funcs[page_name]()
